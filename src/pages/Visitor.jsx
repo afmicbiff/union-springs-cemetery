76378896b7b583d16b2de44f9b57b3d354b22538
anthony handy <ahandyman78897889@gmail.com>
2025-12-13 04:25:00 +0000

    File changes

--- src/pages/Visitor.jsx
+++ src/pages/Visitor.jsx
@@ -31,7 +31,7 @@ export default function VisitorPage() {
 
       {/* Main Content - Quick Access Grid Style */}
       <section className="bg-stone-200 pb-16">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 -mt-[192px] relative z-20">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 -mt-[128px] relative z-20">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             
             {/* Card 1: Hours & Location */}
