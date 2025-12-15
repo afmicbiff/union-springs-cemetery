cc4515722ce812121488a4f6841bf7b9ddafd0ec
anthony handy <ahandyman78897889@gmail.com>
2025-12-15 02:03:13 +0000

    File changes

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -14,24 +14,7 @@ import { Label } from "@/components/ui/label";
 import { Filter, X } from 'lucide-react';
 import Breadcrumbs from "@/components/Breadcrumbs";
 
-// Simple Levenshtein distance for "Did you mean?"
-const getLevenshteinDistance = (a, b) => {
-  if (a.length === 0) return b.length;
-  if (b.length === 0) return a.length;
-  const matrix = [];
-  for (let i = 0; i <= b.length; i++) { matrix[i] = [i]; }
-  for (let j = 0; j <= a.length; j++) { matrix[0][j] = j; }
-  for (let i = 1; i <= b.length; i++) {
-    for (let j = 1; j <= a.length; j++) {
-      if (b.charAt(i - 1) === a.charAt(j - 1)) {
-        matrix[i][j] = matrix[i - 1][j - 1];
-      } else {
-        matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, Math.min(matrix[i][j - 1] + 1, matrix[i - 1][j] + 1));
-      }
-    }
-  }
-  return matrix[b.length][a.length];
-};
+
 
 export default function SearchPage() {
   const [searchTerm, setSearchTerm] = useState('');
