7aca482ccb27bce22425d351fa86af5a8976246b
anthony handy <ahandyman78897889@gmail.com>
2025-12-15 03:07:14 +0000

    File changes

--- src/pages/Memorial.jsx
+++ src/pages/Memorial.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useSearchParams, Link } from 'react-router-dom';
+import { useSearchParams, Link, useLocation } from 'react-router-dom';
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { format } from 'date-fns';
