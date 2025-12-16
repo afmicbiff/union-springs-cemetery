a68f03156e872fac970237acfa5d01b3c00852cd
anthony handy <ahandyman78897889@gmail.com>
2025-12-16 18:24:29 +0000

    File changes

--- src/components/admin/MembersDirectory.jsx
+++ src/components/admin/MembersDirectory.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { base44 } from "@/api/base44Client";
+import { createPageUrl } from '@/utils';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
