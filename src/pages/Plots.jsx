4acd9b7ee8a26774b90d8a1ea8a6ace553b09a09
anthony handy <ahandyman78897889@gmail.com>
2025-12-19 18:04:41 +0000

    File changes

--- src/pages/Plots.jsx
+++ src/pages/Plots.jsx
@@ -3,19 +3,6 @@ import { Upload, Info, Map as MapIcon, Layers, FileText, AlertCircle } from 'luc
 
 // --- MOCK DATA ---
 const INITIAL_CSV = `Grave,Row,Status,Last Name,First Name,Birth,Death,Family Name,Notes,Find A Grave,Section
-1,A-1,Available,,,,,,,,Section 1
-2,A-1,Occupied,Boutwell,Paul Marshall,5/25/1901,9/3/1982,Boutwell,,Find a Grave,Section 1
-3,A-1,Occupied,Boutwell,Clara Martin,11/15/1907,1/31/1995,Boutwell,,Find a Grave,Section 1
-4,B-1,Reserved,,,,,"Slack, Hoyt",,,Section 1
-5,B-1,Reserved,,,,,"Slack, Hoyt",,,Section 1
-6,B-1,Reserved,,,,,"Slack, Hoyt",,,Section 1
-7,B-1,Reserved,,,,,"Slack, Hoyt",,,Section 1
-8,B-1,Occupied,Slack,Zachary Neal,11/11/1984,4/9/1999,Slack,,Find a Grave,Section 1
-9,C-1,Reserved,Slack,Tom L.,7/6/1941,,Slack,,,Section 1
-10,C-1,Occupied,Slack,Pamela D.,12/29/1940,4/2/1999,Slack,,Find a Grave,Section 1
-11,C-1,Occupied,Slack,Hoyt,12/27/1907,1/30/1998,Slack,,Find a Grave,Section 1
-12,C-1,Reserved,Slack,Barbara,,,"Slack, Hoyt",,,Section 1
-13,C-1,Available,,,,,,,,Section 1
 14,D-1,Occupied,Roach,Pauline Dollar,,,,,,,Section 1
 15,D-1,Occupied,Roach,Pauline Dollar,,,,,,,Section 1
 16,D-1,Occupied,Roach,Pauline Dollar,,,,,,,Section 1
@@ -26,6 +13,19 @@ const INITIAL_CSV = `Grave,Row,Status,Last Name,First Name,Birth,Death,Family Na
 21,D-1,Occupied,Dollar,Pauline,,,,,,,Section 1
 22,D-1,Occupied,Rives,"Francis C. ""Jack""",,,,,,,Section 1
 23,D-1,Occupied,Rives,Treable,,,,,,,Section 1
+1,D-1,Available,,,,,,,,Section 1
+2,D-1,Occupied,Boutwell,Paul Marshall,5/25/1901,9/3/1982,Boutwell,,Find a Grave,Section 1
+3,D-1,Occupied,Boutwell,Clara Martin,11/15/1907,1/31/1995,Boutwell,,Find a Grave,Section 1
+4,D-1,Reserved,,,,,"Slack, Hoyt",,,Section 1
+5,D-1,Reserved,,,,,"Slack, Hoyt",,,Section 1
+6,D-1,Reserved,,,,,"Slack, Hoyt",,,Section 1
+7,D-1,Reserved,,,,,"Slack, Hoyt",,,Section 1
+8,D-1,Occupied,Slack,Zachary Neal,11/11/1984,4/9/1999,Slack,,Find a Grave,Section 1
+9,D-1,Reserved,Slack,Tom L.,7/6/1941,,Slack,,,Section 1
+10,D-1,Occupied,Slack,Pamela D.,12/29/1940,4/2/1999,Slack,,Find a Grave,Section 1
+11,D-1,Occupied,Slack,Hoyt,12/27/1907,1/30/1998,Slack,,Find a Grave,Section 1
+12,D-1,Reserved,Slack,Barbara,,,"Slack, Hoyt",,,Section 1
+13,D-1,Available,,,,,,,,Section 1
 `;
 
 // --- CONFIGURATION ---
