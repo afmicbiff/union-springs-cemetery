0c0ad62666907a378caaf91e1f453a4c66b0d4c0
anthony handy <ahandyman78897889@gmail.com>
2025-12-22 04:03:39 +0000

    File changes

--- src/pages/NewPlotDetails.jsx
+++ src/pages/NewPlotDetails.jsx
@@ -273,7 +273,6 @@ export default function NewPlotDetails() {
               </div>
             )}
           </section>
-          )}
 
           {isAdmin && (
           <section className="bg-white rounded-lg border p-5">
