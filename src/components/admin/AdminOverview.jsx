aadc61cbd007de9cd27947b23a7ec2e5d6f78cf1
anthony handy <ahandyman78897889@gmail.com>
2025-12-22 14:50:43 +0000

    File changes

--- src/components/admin/AdminOverview.jsx
+++ src/components/admin/AdminOverview.jsx
@@ -77,7 +77,7 @@ export default function AdminOverview() {
                 </Card>
             </div>
 
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
               <Card className="col-span-1">
                 <CardHeader className="flex flex-row items-center justify-between">
                   <div>
